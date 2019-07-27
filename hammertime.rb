2e466240237dd568520c935ab3f2e9cd11bb9e98
rcabralc <rcabralc@gmail.com>
2019-07-27 14:08:37 -0300

    Lighten dark and darken light backgrounds a bit

--- hammertime.rb
+++ hammertime.rb
@@ -2,8 +2,8 @@ require_relative './undefined'
 
 module Undefined
   Hammertime = Scheme.new(
-    CIELUV.new(13.2, 1.8, -0.6),
-    CIELUV.new(86.8, 12.7, -16.4),
+    CIELUV.new(14, 1.8, -0.6),
+    CIELUV.new(86, 12.7, -16.4),
     red: CIELUV.new(53, 122, -9),
     lime: CIELUV.new(57, -7, 45),
     yellow: CIELUV.new(56, 47, 20),
