44fc6febc669ec45ee7d12d18d345be1e78205e6
rcabralc <rcabralc@gmail.com>
2019-09-08 16:44:04 -0300

    Restore contrast levels in Hammertime
    But keep backgrounds and increased term1-6 contrasts.

--- hammertime.rb
+++ hammertime.rb
@@ -4,12 +4,12 @@ module Undefined
   Hammertime = Scheme.new(
     CIELUV.new(13, 1.8, -0.6),
     CIELUV.new(87, 12.7, -16.4),
-    red: CIELUV.new(59, 97, -20),
-    lime: CIELUV.new(59, -7, 45),
-    yellow: CIELUV.new(59, 47, 20),
-    purple: CIELUV.new(59, 9, -84),
-    orange: CIELUV.new(59, 97, 32),
-    cyan: CIELUV.new(59, -33, 4),
+    red: CIELUV.new(58, 97, -20),
+    lime: CIELUV.new(58, -7, 45),
+    yellow: CIELUV.new(58, 47, 20),
+    purple: CIELUV.new(58, 9, -84),
+    orange: CIELUV.new(58, 97, 32),
+    cyan: CIELUV.new(58, -33, 4),
   )
 
   Hammertime.print if __FILE__ == $0
